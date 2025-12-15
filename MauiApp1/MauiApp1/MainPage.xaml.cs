536c82bef9adfc245cfd22119d7294ac3496d71d
norbertc5 <norbert07c@gmail.com>
2025-12-15 10:19:03 +0100

    powiadomienia

--- MauiApp1/MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiApp1
+﻿using Plugin.LocalNotification;
+
+namespace MauiApp1
 {
     public partial class MainPage : ContentPage
     {
@@ -7,6 +9,42 @@
         public MainPage()
         {
             InitializeComponent();
+            cos();
+        }
+
+        async void cos()
+        {
+            await LocalNotificationCenter.Current.RequestNotificationPermission();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var notification = new NotificationRequest
+            {
+                NotificationId = 100,
+                Title = "Testowe powiadomienie",
+                Description = "Test",
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = DateTime.Now.AddSeconds(5)
+                }
+            };
+
+            LocalNotificationCenter.Current.Show(notification);
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            var notification = new NotificationRequest
+            {
+                NotificationId = 101,
+                Title = "Powiadomienie zaplanowane",
+                Description = "",
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = DatePickerObj.Date.Add(TimePickerObj.Time)
+                }
+            }; LocalNotificationCenter.Current.Show(notification);
         }
     }
 }
--- MauiApp1/MauiApp1/Platforms/Android/MainApplication.cs
+++ MauiApp1/MauiApp1/Platforms/Android/MainApplication.cs
@@ -1,5 +1,16 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.Runtime;
+
+// Essential permissions
+[assembly: UsesPermission(Manifest.Permission.Vibrate)]
+[assembly: UsesPermission(Manifest.Permission.PostNotifications)]
+[assembly: UsesPermission(Manifest.Permission.WakeLock)]
+[assembly: UsesPermission(Manifest.Permission.ReceiveBootCompleted)]
+
+// Optional: For exact timing in Calendar/Alarm apps (Android 14+)
+[assembly: UsesPermission("android.permission.USE_EXACT_ALARM")] // No user prompt, requires Android 13+
+[assembly: UsesPermission("android.permission.SCHEDULE_EXACT_ALARM")] // User can grant permission
 
 namespace MauiApp1
 {
